3d279c30d99b61c95edde70a01ddaf2a46b99b80
GodtierGD <par3ll3l@gmail.com>
2025-03-06 22:42:55 +0000

    Update fish.js

--- commands/others/fish.js
+++ commands/others/fish.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	enabled: true,
-	cooldown: 3600,
+	cooldown: 60,
 	data: new SlashCommandBuilder()
 		.setName('fish')
 		.setDescription('AREDL Fishy'),
